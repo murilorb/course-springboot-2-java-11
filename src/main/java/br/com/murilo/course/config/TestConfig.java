006b88c585987a7df8d0f48a78aa2bc1dd80eb8f
Murilo Rodrigues <murilo.rbarbosa19@gmail.com>
2020-03-03 15:51:59 -0300

    product

--- src/main/java/br/com/murilo/course/config/TestConfig.java
+++ src/main/java/br/com/murilo/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import br.com.murilo.course.entities.Category;
 import br.com.murilo.course.entities.Order;
+import br.com.murilo.course.entities.Product;
 import br.com.murilo.course.entities.User;
 import br.com.murilo.course.entities.enums.OrderStatus;
 import br.com.murilo.course.repositories.CategoryRepository;
 import br.com.murilo.course.repositories.OrderRepository;
+import br.com.murilo.course.repositories.ProductRepository;
 import br.com.murilo.course.repositories.UserRepository;
 
 @Configuration
@@ -29,9 +31,18 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
 		Category cat1 = new Category(null, "Eletronics");
 		Category cat2 = new Category(null, "Books");
@@ -47,6 +58,7 @@ public class TestConfig implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 	}
 	
 }
